3986a592fddfc25255eef588bf1a37fd1d9581a1
manafdabbagh <thedabbagh@gmail.com>
2025-11-20 16:44:30 -0600

    added pause button and user profile

--- app/src/main/java/com/cs407/settlersofmadison/ui/game/GameScreen.kt
+++ app/src/main/java/com/cs407/settlersofmadison/ui/game/GameScreen.kt
@@ -7,12 +7,8 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
